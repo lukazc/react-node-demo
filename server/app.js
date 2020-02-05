ef681719030322688e17142f5ef2967a3ea97167
zac <zac@nivas.hr>
2020-02-05 08:53:06 +0100

    Add authorization middleware

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const authorization = require("./middleware/authorization");
+
 var app = express();
 
 // view engine setup
@@ -19,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(authorization);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
